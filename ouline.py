9d0ab88ba53a0065b6cd0f6669b5ce39711e2ca0
Michael Jean-Jumeau <mjeanjum@uncc.edu>
2022-12-05 00:00:41 -0500

    Updated and completed main in ouline.py, completing Task 4.

--- ouline.py
+++ ouline.py
@@ -1,15 +1,22 @@
 #import beautifulsoup and request here
-
+from bs4 import BeautifulSoup
+import requests
+import json
 
 #function to get job list from url f'https://www.talent.com/jobs?k={role}&l={location}'
 
 def getJobList(role,location):
     url = f'https://www.talent.com/jobs?k={role}&l={location}'
     # Complete the missing part of this function here
+    response = requests.get(url, params={"k": role, "l": location})
+    return response.text
 
 #save data in JSON file
 def saveDataInJSON(jobDetails):
     #Complete the missing part of this function here
+    json_file = open("Available_Jobs.json", "w")
+    json.dump(jobDetails, json_file)
+    json_file.close()
     print("Saving data to JSON")
 
 #main function
@@ -18,6 +25,17 @@ def main():
     print("Enter role you want to search")
     role = input()
     # Complete the missing part of this function here
+    print("Enter desired location")
+    location = input()
+
+    jobDetails = getJobList(role, location)
+    #job_list_file = 
+    saveDataInJSON(jobDetails)
+    #f = open(job_list_file, 'r')
+    #job_list = f.read()
+
+    #print("Available " + role + " roles: \n" + job_list)
+    #f.close()
     
 if __name__ == '__main__':
     main()
